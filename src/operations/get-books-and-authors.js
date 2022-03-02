bedd03dad69b483925be0e65ab75839eafd51973
p-bodson <59319428+p-bodson@users.noreply.github.com>
2022-03-01 21:49:05 -0800

    filtered search by ANDING parameters

--- src/operations/get-books-and-authors.js
+++ src/operations/get-books-and-authors.js
@@ -4,14 +4,30 @@ import pool from '../db-connector.js'
 
 const get_books_and_authors = async (req) => {
 
-    const { isbn,
+    // establish the shema values to be returned from the query
+    const schema_keys = ["isbn", 
+        "book_title", 
+        "author_name", 
+        "author_ID"]
+
+    // these dereferences should match schema keys exactly
+    let { isbn,
         book_title, 
         author_name, 
         author_ID } = req.query;
 
-    let res = {}
-    let conn;
-
+    // search for good keys
+    const query_keys = Object.keys(req.query);
+    const good_keys = query_keys.filter(key => {
+        for (const item of schema_keys) {
+            if (key === item) {
+                return true;
+            }
+        }
+        return false;
+    })
+    
+    // build the query based on query parameters
     let query = `SELECT b.isbn, 
         b.book_title, 
         a.author_name, 
@@ -20,28 +36,44 @@ const get_books_and_authors = async (req) => {
         JOIN Books_Authors AS ba 
         ON b.isbn=ba.isbn 
         RIGHT JOIN Authors AS a  
-        ON ba.author_id=a.author_id`
+        ON ba.author_id=a.author_id`;
 
-    // build the query based on query parameters
+    const parameter_length = good_keys.length;
+    if ( parameter_length !== 0 ) query += ` WHERE`
 
-    if (isbn) {
-        query = query + ` WHERE b.isbn=${'"' + isbn + '"'}`
+    // go through the good keys and add them to the query
+    for (let i = 0; i < parameter_length; i++) {        
+        // set the criteria to null after a match is found
+        // to avoid adding it twice.
+        if (isbn) {
+            query += ` b.isbn="${isbn}"`;
+            isbn = null;
+        }
+        else if (book_title) {
+            query += ` b.book_title="${book_title}"`;
+            book_title = null;
+        }
+        else if (author_name) {
+            query += ` a.author_name="${author_name}"`;
+            author_name = null;
+        }
+        else if (author_ID) {
+            query += ` a.author_ID="${author_ID}"`;
+            author_ID = null;
+        }        
+
+        // AND the filters together
+        if ( i < parameter_length - 1) {
+          query += ` AND`
+        }
     }
-    //if (book_title) {
-    //    query = query + ` b.book_title=${book_title}`
-    //}
-    //if (author_name) {
-    //    query = query + ` a.author_name=${author_name}`
-    //}
-    //if (author_ID) {
-    //    query = query + ` a.author_ID=${author_ID}`
-    //}
 
     // add the ordering
-    query = query + ` ORDER BY b.book_title ASC;`
+    query += ` ORDER BY b.book_title ASC;`
 
-    console.log(query);
-
+    // start the database access
+    let res = {}
+    let conn;
     try {
         conn = await pool.getConnection()
         res = await conn.query(query)
