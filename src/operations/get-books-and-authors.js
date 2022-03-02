eb2c42008797c346f91f5294fe767b8dad82d27e
p-bodson <59319428+p-bodson@users.noreply.github.com>
2022-03-01 18:50:00 -0800

    searches with a single query

--- src/operations/get-books-and-authors.js
+++ src/operations/get-books-and-authors.js
@@ -3,10 +3,16 @@
 import pool from '../db-connector.js'
 
 const get_books_and_authors = async (req) => {
+
+    const { isbn,
+        book_title, 
+        author_name, 
+        author_ID } = req.query;
+
     let res = {}
     let conn;
 
-    const query = `SELECT b.isbn, 
+    let query = `SELECT b.isbn, 
         b.book_title, 
         a.author_name, 
         a.author_ID 
@@ -14,8 +20,27 @@ const get_books_and_authors = async (req) => {
         JOIN Books_Authors AS ba 
         ON b.isbn=ba.isbn 
         RIGHT JOIN Authors AS a  
-        ON ba.author_id=a.author_id 
-        ORDER BY b.book_title ASC;` 
+        ON ba.author_id=a.author_id`
+
+    // build the query based on query parameters
+
+    if (isbn) {
+        query = query + ` WHERE b.isbn=${'"' + isbn + '"'}`
+    }
+    //if (book_title) {
+    //    query = query + ` b.book_title=${book_title}`
+    //}
+    //if (author_name) {
+    //    query = query + ` a.author_name=${author_name}`
+    //}
+    //if (author_ID) {
+    //    query = query + ` a.author_ID=${author_ID}`
+    //}
+
+    // add the ordering
+    query = query + ` ORDER BY b.book_title ASC;`
+
+    console.log(query);
 
     try {
         conn = await pool.getConnection()
