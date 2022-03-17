afe71464c597e427a440cd44b95e8ecdd07f6f16
Kevin Peralta <peraltak@oregonstate.edu>
2022-03-16 19:56:11 -0700

    fixed update file for rental_items

--- src/operations/put-rental-items.js
+++ src/operations/put-rental-items.js
@@ -8,9 +8,7 @@ const put_rental_items = async (req) => {
     let { rental_ID, resource_ID, queue_numb, rental_item_status, return_date } = req.body;
 
     let query = `UPDATE Rental_Items
-    SET rental_ID="${rental_ID}", 
-    resource_ID="${resource_ID}", 
-    queue_numb="${queue_numb}",
+    SET queue_numb="${queue_numb}",
     rental_item_status="${rental_item_status}", 
     return_date="${return_date}"
     WHERE rental_ID=${rental_ID} AND resource_ID="${resource_ID}";`;
