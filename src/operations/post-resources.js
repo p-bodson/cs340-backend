1b2e008194d8a41314831f7ac6ad83c1719b6d83
p-bodson <59319428+p-bodson@users.noreply.github.com>
2022-03-17 11:49:21 -0700

    changing post-resources to change quantity_checked_out as well

--- src/operations/post-resources.js
+++ src/operations/post-resources.js
@@ -6,10 +6,10 @@ const post_resources = async (req) => {
     // this function CREATEs a new book in the Books table
 
     // first build the query
-    let { isbn, library_ID, quantity_available } = req.body;
+    let { isbn, library_ID, quantity_available, quantity_checked_out } = req.body;
     let query = `INSERT INTO Resources (isbn, library_ID, 
         quantity_available, quantity_checked_out)
-        VALUES ("${isbn}", ${library_ID}, ${quantity_available}, 0);`;
+        VALUES ("${isbn}", ${library_ID}, ${quantity_available}, ${quantity_checked_out});`;
 
     // then do the query
     return do_query(query);
